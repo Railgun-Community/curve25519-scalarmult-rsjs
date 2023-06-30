8464d4e1b4f21780de969890bf6f0fab9633a51d
Andre Staltz <andre@staltz.com>
2023-06-30 18:02:59 +0300

    check ELF of each prebuild

--- tools/check-prebuilds.js
+++ tools/check-prebuilds.js
@@ -1,15 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
+const exec = require('node:child_process').execSync;
 
 const files = [
-  'prebuilds/android-arm/index.node',
-  'prebuilds/android-arm64/index.node',
-  'prebuilds/android-x64/index.node',
-  'prebuilds/ios-arm64/index.node/index',
-  'prebuilds/ios-x64/index.node/index',
+  ['prebuilds/android-arm/index.node', 'ELF 32-bit ARM'],
+  ['prebuilds/android-arm64/index.node', 'ELF 64-bit ARM aarch64'],
+  ['prebuilds/android-x64/index.node', 'ELF 64-bit x86-64'],
+  ['prebuilds/ios-arm64/index.node/index', 'Mach-O 64-bit arm64'],
+  ['prebuilds/ios-x64/index.node/index', 'Mach-O 64-bit x86_64'],
 ];
 
-for (const file of files) {
+for (const [file, expected] of files) {
+  // Check if file exists
   const parts = file.split('/');
   for (let i = 1; i < parts.length; i++) {
     const partialFilename = path.resolve(
@@ -24,5 +26,16 @@ for (const file of files) {
   }
   if (!fs.existsSync(file)) {
     console.error('Cannot npm publish with missing ' + file);
+  }
+
+  // Check if file passes ELF or Mach-O test
+  const output = exec(`file ${file}`).toString();
+  const expectedWords = expected.split(' ');
+  for (const expectedWord of expectedWords) {
+    if (!output.includes(expectedWord)) {
+      console.error(
+        `Cannot npm publish with ${file} not matching ${expected}:\n${output}`,
+      );
+    }
   }
 }
